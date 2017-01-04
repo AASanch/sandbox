97a23fc01ae61c24e026f5ab95236855ba56eb64
Sanchez <adrian.sanchez@emerson.com>
2017-01-04 17:51:25 -0600

    Added gulpfile and angular dependencies.

--- 01_OpenGameList/gulpfile.js
+++ 01_OpenGameList/gulpfile.js
@@ -1,45 +1,69 @@
-/// <binding Clean='clean' />
-"use strict";
-
 var gulp = require("gulp"),
-    rimraf = require("rimraf"),
+    clean = require("gulp-clean"),
     concat = require("gulp-concat"),
-    cssmin = require("gulp-cssmin"),
+    sourcemaps = require("gulp-sourcemaps"),
+    typescript = require("gulp-typescript"),
     uglify = require("gulp-uglify");
 
-var webroot = "./wwwroot/";
-
-var paths = {
-    js: webroot + "js/**/*.js",
-    minJs: webroot + "js/**/*.min.js",
-    css: webroot + "css/**/*.css",
-    minCss: webroot + "css/**/*.min.css",
-    concatJsDest: webroot + "js/site.min.js",
-    concatCssDest: webroot + "css/site.min.css"
+//Paths
+var webroot = "./wwwroot";
+var tsconfig = "./tsconfig.json";
+var srcPaths = {
+    app: ["Scripts/app/**/*.ts"],
+    js: [
+        "Scripts/app/**/*.js",
+        "node_modules/@angular/**",
+        "node_modules/@rxjs/**",
+        "node_modules/core-js/client/shim.min.js",
+        'node_modules/zone.js/dist/zone.js',
+        'node_modules/reflect-metadata/Reflect.js',
+        'node_modules/systemjs/dist/system.src.js',
+        'node_modules/typescript/lib/typescript.js'],
+    html: ["Scripts/app/**/*.html"],
+    css: ["Scripts/app/**/*.css"]
 };
 
-gulp.task("clean:js", function (cb) {
-    rimraf(paths.concatJsDest, cb);
+var destPaths = {
+    app: `${webroot}/app/`,
+    js: `${webroot}/js/`
+};
+
+//cleans the wwwroot/app folder
+gulp.task("clean:app", function() {
+    return gulp.src(destPaths.app)
+        .pipe(clean({ force: true }));
 });
 
-gulp.task("clean:css", function (cb) {
-    rimraf(paths.concatCssDest, cb);
+//cleans the wwwroot/js folder
+gulp.task("clean:js", function() {
+    return gulp.src(destPaths.js)
+        .pipe(clean({ force: true }));
 });
 
-gulp.task("clean", ["clean:js", "clean:css"]);
+//clean all within wwwroot
+gulp.task("clean", ["clean:app", "clean:js"]);
 
-gulp.task("min:js", function () {
-    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
-        .pipe(concat(paths.concatJsDest))
-        .pipe(uglify())
-        .pipe(gulp.dest("."));
+//copies all js files from external libraries to wwwroot/js
+gulp.task("copy:js", ["clean:js"], function() {
+    return gulp.src(srcPaths.js)
+        .pipe(gulp.dest(destPaths.js));
 });
 
-gulp.task("min:css", function () {
-    return gulp.src([paths.css, "!" + paths.minCss])
-        .pipe(concat(paths.concatCssDest))
-        .pipe(cssmin())
-        .pipe(gulp.dest("."));
+//compile, minify, and create sourcemaps and place them to wwwroot/app.
+gulp.task("build:app", ["clean:app"], function() {
+    return gulp.src(srcPaths.app)
+        .pipe(sourcemaps.init())
+        .pipe(typescript(require(tsconfig).compilerOptions))
+        .pipe(uglify({ mangle: false }))
+        .pipe(sourcemaps.write('/'))
+        .pipe(gulp.dest(destPaths.app));
 });
 
-gulp.task("min", ["min:js", "min:css"]);
+//Watch specified files and define what to do upon file changes.
+gulp.task("watch", function() {
+    return gulp.watch([srcPaths.app, srcpaths.js], ['build:app', 'copy:js']);
+})
+
+//global build task
+gulp.task("build", ["clean", "copy:js", "build:app"]);
+
